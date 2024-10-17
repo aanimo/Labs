6c04a028703bb5e3ff043797f076c8d2656ff189
Шмалько Иван <e.sh@list.ru>
2024-10-17 19:28:43 +0300

    Взял за основу исправленную 1 лабу

--- CSharpLabs/lab2/Program.cs
+++ CSharpLabs/lab2/Program.cs
@@ -1,5 +1,104 @@
 ﻿using System;
 
+class Student
+{
+    private string _name;
+    public int Age { get; set; }
+    public Subject FavoriteSubject { get; set; } // Поле public
+    private Game FavoriteGame; // Поле private
+
+    public Student(string name, int Age, Subject favoriteSubject, Game favoriteGame)
+    {
+        this._name = name;
+        this.Age = Age;
+        this.FavoriteSubject = favoriteSubject;
+        this.FavoriteGame = favoriteGame;
+    }
+
+    public Student(string name, Subject favoriteSubject, Game favoriteGame)
+    {
+        this._name = name;
+        Age = 0;
+        this.FavoriteSubject = favoriteSubject;
+        this.FavoriteGame = favoriteGame;
+    }
+
+    public string WriteInfo()
+    {
+        return "Студент " + _name + ", возраст " + Age;
+    }
+
+    public void BecomeOlder()
+    {
+        Age++;
+        Console.WriteLine("Студент " + _name + ", возраст " + Age);
+    }
+
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public void SetName(string name)
+    {
+        this._name = name;
+    }
+
+    // Метод для получения любимых предмета и игры
+    public string GetFavorites()
+    {
+        return $"Любимый предмет: {FavoriteSubject.Name}, любимая игра: {FavoriteGame.GetName()}";
+    }
+
+    // Метод для демонстрации передачи параметра по ссылке
+    public static void ChangeStudentName(ref Student student, string newName)
+    {
+        student.SetName(newName);
+    }
+
+    // Метод для демонстрации передачи параметра по значению
+    public static void ChangeAgeByValue(int age)
+    {
+        age += 1; // Изменение произойдет только в локальной копии
+    }
+
+    static void Main(string[] args)
+    {
+        // Создаем объекты любимого предмета и игры
+        Subject math = new("Математика");
+        Game football = new("Футбол");
+
+        // Создаем студентов
+        Student Oleg = new("Олег", 25, math, football);
+        Student Venya = new("Веня", math, football);
+
+        // Демонстрация работы WriteInfo и BecomeOlder
+        Console.WriteLine(Oleg.WriteInfo());
+        Oleg.BecomeOlder();
+
+        Console.WriteLine(Venya.WriteInfo());
+        Oleg.SetName("Рома");
+
+        // Повторный вызов после изменения имени
+        Console.WriteLine(Oleg.WriteInfo());
+
+        // Демонстрация работы геттера GetName
+        Console.WriteLine("Имя студента: " + Oleg.GetName());
+
+        // Демонстрация получения любимых предмета и игры
+        Console.WriteLine(Oleg.GetFavorites());
+
+        // Демонстрация передачи параметра по значению
+        int age = Oleg.Age;
+        ChangeAgeByValue(age);
+        Console.WriteLine($"Возраст после попытки изменения по значению: {Oleg.Age}");
+
+        // Демонстрация передачи параметра по ссылке
+        ChangeStudentName(ref Oleg, "Андрей");
+        Console.WriteLine($"Имя после изменения по ссылке: {Oleg.GetName()}");
+    }
+}
+
 class Subject
 {
     public string Name { get; set; }
@@ -12,114 +111,16 @@ class Subject
 
 class Game
 {
-    public string Title { get; set; }
+    private string name; // Поле private
 
-    public Game(string title)
-    {
-        Title = title;
-    }
-}
-
-class Student
-{
-    private string name; // Поле с модификатором private
-    public int Age { get; set; } // Поле с модификатором public
-    public Subject FavoriteSubject { get; set; }
-    public Game FavoriteGame { get; set; }
-
-    // Конструктор с двумя параметрами
-    public Student(string name, int age, Subject favoriteSubject, Game favoriteGame)
+    public Game(string name)
     {
         this.name = name;
-        Age = age;
-        FavoriteSubject = favoriteSubject;
-        FavoriteGame = favoriteGame;
     }
 
-    // Конструктор с одним параметром
-    public Student(string _name)
-    {
-        name = _name;
-        Age = 0;
-        FavoriteSubject = new Subject("Неизвестный предмет");
-        FavoriteGame = new Game("Неизвестная игра");
-    }
-
-    // Метод для вывода информации о студенте
-    public void WriteInfo()
-    {
-        Console.WriteLine("Студент " + name + ", возраст " + Age);
-    }
-
-    // Метод для увеличения возраста студента
-    public void BecomeOlder()
-    {
-        Console.WriteLine("Студент " + name + ", возраст " + (Age + 1));
-    }
-
-    // Метод для получения имени студента
+    // Метод для получения имени игры
     public string GetName()
     {
         return name;
-    }
-
-    // Метод для установки имени студента
-    public void SetName(string _name)
-    {
-        name = _name;
-    }
-
-    // Метод, возвращающий любимый предмет и игру студента
-    public void GetFavoriteSubjectAndGame()
-    {
-        Console.WriteLine("Любимый предмет: " + FavoriteSubject.Name);
-        Console.WriteLine("Любимая игра: " + FavoriteGame.Title);
-    }
-
-    // Пример передачи параметра по значению
-    public static void ModifyStudentByValue(Student student)
-    {
-        student = new Student("Измененный студент", 30, new Subject("Физика"), new Game("Шахматы"));
-    }
-
-    // Пример передачи параметра по ссылке
-    public static void ModifyStudentByReference(ref Student student)
-    {
-        student.name = "Студент по ссылке";
-        student.Age = 35;
-    }
-
-    static void Main(string[] args)
-    {
-        // Создаем объекты студента
-        Subject math = new Subject("Математика");
-        Game football = new Game("Футбол");
-
-        Student Oleg = new Student("Олег", 25, math, football);
-        Student Venya = new Student("Веня");
-
-        // Вывод информации о студентах
-        Oleg.WriteInfo();
-        Oleg.BecomeOlder();
-
-        Venya.WriteInfo();
-        Oleg.SetName("Рома");
-        Oleg.BecomeOlder();
-
-        // Вывод любимого предмета и игры
-        Oleg.GetFavoriteSubjectAndGame();
-        Venya.GetFavoriteSubjectAndGame();
-
-        // Пример передачи параметра по значению
-        Console.WriteLine("\nПередача по значению:");
-        ModifyStudentByValue(Oleg);
-        Oleg.WriteInfo(); // Имя и возраст не изменятся
-
-        // Пример передачи параметра по ссылке
-        Console.WriteLine("\nПередача по ссылке:");
-        ModifyStudentByReference(ref Oleg);
-        Oleg.WriteInfo(); // Имя и возраст изменятся
-
-
     }
 }
