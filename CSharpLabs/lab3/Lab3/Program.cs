2d7234e3d586cbc72c6d7da48841a091e2f72cf3
Шмалько Иван <e.sh@list.ru>
2024-10-17 19:16:39 +0300

    lab3

--- CSharpLabs/lab3/Lab3/Program.cs
+++ CSharpLabs/lab3/Lab3/Program.cs
@@ -4,42 +4,69 @@ class Student
 {
     private string name;
     public int Age { get; set; }
-    public Student(string name, int Age)
+
+    // Статическое поле
+    public static int TotalStudents;
+
+    // Статическое свойство
+    public static string University { get; set; }
+
+    // Статический метод
+    public static void DisplayTotalStudents()
     {
-        this.name = name;
-        this.Age = Age;
+        Console.WriteLine($"Всего студентов: {TotalStudents}");
     }
 
-    public Student(string _name)
+    // Статический конструктор
+    static Student()
     {
-        this.name = _name;
+        University = "СГУ";
+        TotalStudents = 0;
+    }
+
+    public Student(string name, int age)
+    {
+        this.name = name;
+        this.Age = age;
+        TotalStudents++; // Увеличение количества студентов при создании объекта
+    }
+
+    public Student(string name)
+    {
+        this.name = name;
         Age = 0;
+        TotalStudents++; // Увеличение количества студентов при создании объекта
     }
 
     public void WriteInfo()
     {
-        Console.WriteLine("Студент " + name + ", возраст " + Age);
+        Console.WriteLine($"Студент {name}, возраст {Age}, университет: {University}");
     }
+
     public void BecomeOlder()
     {
-        Console.WriteLine("Студент " + name + ", возраст " + (Age + 1));
+        Console.WriteLine($"Студент {name}, возраст {Age + 1}");
     }
 
     public string GetName()
     {
         return name;
     }
+
     public void SetName(string _name)
     {
         this.name = _name;
     }
 
-
+    // Тестирование программы
     static void Main(string[] args)
     {
+        // Создание объектов разными способами
         Student Oleg = new("Олег", 25);
         Student Venya = new("Веня");
+        Student Anonymous = new Student("Неизвестный");
 
+        // Использование методов
         Oleg.WriteInfo();
         Oleg.BecomeOlder();
 
@@ -47,6 +74,22 @@ class Student
         Oleg.SetName("Рома");
         Oleg.BecomeOlder();
 
+        // Использование статического метода
+        Student.DisplayTotalStudents();
+
+        // Создание статического класса и взаимодействие
+        StudentHelper.GraduationMessage(Oleg);
+        StudentHelper.GraduationMessage(Venya);
+
+        Console.ReadLine();
     }
 }
 
+// Статический класс
+static class StudentHelper
+{
+    public static void GraduationMessage(Student student)
+    {
+        Console.WriteLine($"{student.GetName()} закончил университет {Student.University}.");
+    }
+}
