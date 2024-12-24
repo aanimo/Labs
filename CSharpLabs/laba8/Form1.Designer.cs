05f32c843ad0aec1f9388b2e53d0d877b82fad5a
Шмалько Иван <e.sh@list.ru>
2024-12-25 01:38:06 +0300

    .

--- CSharpLabs/laba8/Form1.Designer.cs
+++ CSharpLabs/laba8/Form1.Designer.cs
@@ -38,6 +38,12 @@
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
             this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridViewStudents)).BeginInit();
             this.SuspendLayout();
             // 
@@ -48,57 +54,58 @@
             this.dataGridViewStudents.Name = "dataGridViewStudents";
             this.dataGridViewStudents.RowHeadersWidth = 62;
             this.dataGridViewStudents.RowTemplate.Height = 28;
-            this.dataGridViewStudents.Size = new System.Drawing.Size(776, 261);
+            this.dataGridViewStudents.Size = new System.Drawing.Size(875, 261);
             this.dataGridViewStudents.TabIndex = 0;
             // 
             // recordBookTextBox
             // 
-            this.recordBookTextBox.Location = new System.Drawing.Point(71, 321);
+            this.recordBookTextBox.Location = new System.Drawing.Point(12, 321);
+            this.recordBookTextBox.MaxLength = 8;
             this.recordBookTextBox.Name = "recordBookTextBox";
-            this.recordBookTextBox.Size = new System.Drawing.Size(100, 26);
+            this.recordBookTextBox.Size = new System.Drawing.Size(158, 26);
             this.recordBookTextBox.TabIndex = 1;
             // 
             // fullNameTextBox
             // 
-            this.fullNameTextBox.Location = new System.Drawing.Point(71, 377);
+            this.fullNameTextBox.Location = new System.Drawing.Point(12, 397);
             this.fullNameTextBox.Name = "fullNameTextBox";
-            this.fullNameTextBox.Size = new System.Drawing.Size(100, 26);
+            this.fullNameTextBox.Size = new System.Drawing.Size(158, 26);
             this.fullNameTextBox.TabIndex = 2;
             // 
             // groupTextBox
             // 
-            this.groupTextBox.Location = new System.Drawing.Point(431, 368);
+            this.groupTextBox.Location = new System.Drawing.Point(479, 395);
             this.groupTextBox.Name = "groupTextBox";
-            this.groupTextBox.Size = new System.Drawing.Size(100, 26);
+            this.groupTextBox.Size = new System.Drawing.Size(200, 26);
             this.groupTextBox.TabIndex = 3;
             // 
             // departmentComboBox
             // 
             this.departmentComboBox.FormattingEnabled = true;
-            this.departmentComboBox.Location = new System.Drawing.Point(226, 321);
+            this.departmentComboBox.Location = new System.Drawing.Point(233, 319);
             this.departmentComboBox.Name = "departmentComboBox";
-            this.departmentComboBox.Size = new System.Drawing.Size(121, 28);
+            this.departmentComboBox.Size = new System.Drawing.Size(179, 28);
             this.departmentComboBox.TabIndex = 4;
             this.departmentComboBox.SelectedIndexChanged += new System.EventHandler(this.DepartmentComboBox_SelectedIndexChanged);
             // 
             // specificationComboBox
             // 
             this.specificationComboBox.FormattingEnabled = true;
-            this.specificationComboBox.Location = new System.Drawing.Point(226, 377);
+            this.specificationComboBox.Location = new System.Drawing.Point(233, 395);
             this.specificationComboBox.Name = "specificationComboBox";
-            this.specificationComboBox.Size = new System.Drawing.Size(121, 28);
+            this.specificationComboBox.Size = new System.Drawing.Size(179, 28);
             this.specificationComboBox.TabIndex = 5;
             // 
             // dateOfAdmissionPicker
             // 
-            this.dateOfAdmissionPicker.Location = new System.Drawing.Point(431, 321);
+            this.dateOfAdmissionPicker.Location = new System.Drawing.Point(479, 321);
             this.dateOfAdmissionPicker.Name = "dateOfAdmissionPicker";
             this.dateOfAdmissionPicker.Size = new System.Drawing.Size(200, 26);
             this.dateOfAdmissionPicker.TabIndex = 6;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(662, 279);
+            this.button1.Location = new System.Drawing.Point(748, 313);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(126, 34);
             this.button1.TabIndex = 7;
@@ -108,7 +115,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(662, 321);
+            this.button2.Location = new System.Drawing.Point(748, 355);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(126, 38);
             this.button2.TabIndex = 8;
@@ -118,7 +125,7 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(662, 365);
+            this.button3.Location = new System.Drawing.Point(748, 399);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(126, 29);
             this.button3.TabIndex = 9;
@@ -126,11 +133,71 @@
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.DeleteStudent);
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 295);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(160, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "№ Зачетной книжки";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 374);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(47, 20);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "ФИО";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(229, 293);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Институт";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(229, 368);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(112, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "Направление";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(475, 298);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(141, 20);
+            this.label5.TabIndex = 14;
+            this.label5.Text = "Дата зачисления";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(475, 370);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 20);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "№ Группы";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(899, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -161,6 +228,12 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
